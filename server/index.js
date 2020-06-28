aa323f170919c0d53d42cf9d482eb4c0413f39b0
TK1351 <takakikitsui0111@gmail.com>
2020-06-28 11:55:37 +0900

    prod用DB作成

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const config = require('./config/dev')
+const config = require('./config/')
 const mongoose = require('mongoose')
 
 const articleRoutes = require('./routes/article')
@@ -28,10 +28,13 @@ app.use('/api/v1/article', articleRoutes)
 app.use('/api/v1/users', usersRoutes)
 app.use('/api/v1/search', searchRoutes)
 
-const appPath = path.join(__dirname,'..','dist','movie-app')
-app.use(express.static(appPath))
-app.get('*', function(req,res){
-  res.sendFile(path.resolve(appPath, 'index.html'))
+if(process.env.NODE_ENV === 'prod'){
+  const appPath = path.join(__dirname,'..','dist','movie-app')
+  app.use(express.static(appPath))
+  app.get('*', function(req,res){
+    res.sendFile(path.resolve(appPath, 'index.html'))
 })
+}
+
 
 app.listen(3000, () => console.log('I am running'))
