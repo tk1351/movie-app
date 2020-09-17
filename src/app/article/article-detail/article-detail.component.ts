dade527fe4a7e2501a5c3c03dfdb7a67371151de
kotono.ukai <helloiamktn@gmail.com>
2020-09-18 07:10:38 +0900

    いろいろ修正

--- src/app/article/article-detail/article-detail.component.ts
+++ src/app/article/article-detail/article-detail.component.ts
@@ -1,59 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router'
-import { ArticleService } from '../shared/article.service';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/shared/auth.service';
+import { ArticleService } from '../shared/article.service';
 
 @Component({
   selector: 'app-article-detail',
   templateUrl: './article-detail.component.html',
-  styleUrls: ['./article-detail.component.scss']
+  styleUrls: ['./article-detail.component.scss'],
 })
 export class ArticleDetailComponent implements OnInit {
-  article
-  updateform
-  users
-  value = ''
-  username
+  article;
+  updateform;
+  users;
+  value = '';
+  username;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     public articleService: ArticleService,
     public auth: AuthService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.get()
-    this.getUser()
+    this.get();
+    this.getUser();
   }
 
   get(): void {
-    this.route.paramMap.subscribe(params => {
-      const articleObservable = this.articleService.getArticleById
-      (params.get('id'))
+    this.route.paramMap.subscribe((params) => {
+      const articleObservable = this.articleService.getArticleById(
+        params.get('id')
+      );
       articleObservable.subscribe(
         (data) => {
-          this.article = data
+          this.article = data;
         },
         (err) => {
-          console.log('失敗')
+          console.log('失敗');
         }
-      )
-    })
+      );
+    });
   }
 
   delete(): void {
-    this.route.paramMap.subscribe(params => {
-      this.articleService.deleteArticleById(params.get('id'))
-        .subscribe(() => this.router.navigate(['/article']))
-    })
+    this.route.paramMap.subscribe((params) => {
+      this.articleService
+        .deleteArticleById(params.get('id'))
+        .subscribe(() => this.router.navigate(['/article']));
+    });
   }
 
   getUser(): void {
-    this.articleService.getUsers()
-      .subscribe(users => {
-      this.username = users.filter(user => user._id === this.article.uid)[0].username
-    })
+    this.articleService.getUsers().subscribe((users) => {
+      this.username = users.filter(
+        (user) => user._id === this.article.uid
+      )[0].username;
+    });
   }
-
 }
--- src/app/article/mypage/article-form/article-form.component.ts
+++ src/app/article/mypage/article-form/article-form.component.ts
@@ -1,37 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { ArticleService } from '../../shared/article.service';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/shared/auth.service';
+import { ArticleService } from '../../shared/article.service';
 
 @Component({
   selector: 'app-article-form',
   templateUrl: './article-form.component.html',
-  styleUrls: ['./article-form.component.scss']
+  styleUrls: ['./article-form.component.scss'],
 })
 export class ArticleFormComponent implements OnInit {
-
   constructor(
     private articleService: ArticleService,
     public auth: AuthService,
     private router: Router
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   post(articleForm) {
-    const uid = localStorage.getItem("userId")
-    const postValue = {...articleForm.value, uid: uid}
-    
+    const uid = localStorage.getItem('userId');
+    const postValue = { ...articleForm.value, uid: uid };
+
     this.articleService.postArticle(postValue).subscribe(
       (result) => {
-        console.log('success')
-        this.router.navigate(['/article'])
+        console.log('success');
+        this.router.navigate(['/article']);
       },
       (err) => {
-        console.error(err)
+        console.error(err);
       }
-    )
+    );
   }
-
 }
